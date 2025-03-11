0b793b3fceb66ed9d6ca511676c3540efbd7714a
Shai Levi <slevi@redhat.com>
2025-03-11 17:48:04 +0200

    Edit Reconcile comments

--- internal/controller/githubissue_controller.go
+++ internal/controller/githubissue_controller.go
@@ -46,7 +46,7 @@ type GithubIssueReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
+// Reconcile function compare the state specified by
 // the GithubIssue object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
