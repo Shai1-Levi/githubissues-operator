1ee6fe28226a45e1052d1754cccc7f3e91bc78f7
Shai Levi <slevi@redhat.com>
2025-03-17 09:12:43 +0200

    commit temp situation, before adding finalizare to to catch the CR before deleteion, Helps to handle closing issue

--- internal/controller/githubissue_controller.go
+++ internal/controller/githubissue_controller.go
@@ -25,15 +25,16 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	trainingv1alpha1 "Shai1-Levi/githubissues-operator.git/api/v1alpha1"
+	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
+	// "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	// "k8s.io/apimachinery/pkg/runtime/schema"
+	// "k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 	"net/http"
 	"os"
@@ -94,38 +95,7 @@ func (r *GithubIssueReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	fmt.Print("Found")
 	fmt.Print(len(crdList.Items))
 
-	// Create dynamic client
-	dynClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		fmt.Println("Error creating dynamic client:", err)
-		os.Exit(1)
-	}
-
-	// Define the GroupVersionResource (GVR) for your CRD
-	gvr := schema.GroupVersionResource{
-		Group:    "training.redhat.com",
-		Version:  "v1alpha1",
-		Resource: "githubissues", // Plural form of your CRD
-	}
-
-	// Fetch a specific CR instance (replace "githubissue-sample" with your CR name)
-	crName := "example-issue"
-	cr, err := dynClient.Resource(gvr).Namespace("default").Get(context.TODO(), crName, metav1.GetOptions{})
-	if err != nil {
-		fmt.Println("Error fetching Custom Resource:", err)
-		os.Exit(1)
-	}
-
-	// Extract `spec` field
-	title, found, err := unstructured.NestedString(cr.Object, "spec", "title")
-	description, found, err := unstructured.NestedString(cr.Object, "spec", "description")
-	if err != nil || !found {
-		fmt.Println("Error retrieving spec:", err)
-		os.Exit(1)
-	}
-
-	// Print the spec content
-	fmt.Println("Spec of", crName, ":", title)
+	// #################################### the code above is not needed
 
 	// Fetch issues from GitHub
 	body, err := r.fetchGitHubIssues()
@@ -134,39 +104,226 @@ func (r *GithubIssueReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		return ctrl.Result{}, nil
 	}
 
-	// Define a generic map
-	var result []map[string]interface{}
+	// Define a generic map for GitHubIssues
+	var GitHubIssues []map[string]interface{}
 
 	// Parse the JSON
-	err = json.Unmarshal(body, &result)
+	err = json.Unmarshal(body, &GitHubIssues)
 	if err != nil {
 		log.Info("err\n")
 	}
 
-	var i int
+	// var i int
 
+	// // Print all keys and values dynamically
+	// for i = 0; i < len(GitHubIssues); i++ {
+	// 	item := GitHubIssues[i]
+	// 	fmt.Printf("\nIssue %d:\n", i)
+	// 	titleStr := fmt.Sprintf(item["title"].(string))
+	// 	isOpen := fmt.Sprintf(item["state"].(string))
+	// 	// url := fmt.Sprintf(item["url"].(string))
+	// 	if (strings.TrimRight(string(titleStr), "\n") == title) && isOpen == "open" {
+	// 		break
+	// 	}
+	// }
+
+	// // Create dynamic client
+	// dynClient, err := dynamic.NewForConfig(config)
+	// if err != nil {
+	// 	fmt.Println("Error creating dynamic client:", err)
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// // Define the GroupVersionResource (GVR) for your CRD
+	// gvr := schema.GroupVersionResource{
+	// 	Group:    "training.redhat.com",
+	// 	Version:  "v1alpha1",
+	// 	Resource: "githubissues", // Plural form of your CRD
+	// }
+
+	// // Fetch a specific CR instance (replace "githubissue-sample" with your CR name)
+	// crName := "example-issue" // TODO: $###########################################################################################################3
+	// cr, err := dynClient.Resource(gvr).Namespace("default").Get(context.TODO(), crName, metav1.GetOptions{})
+	// if err != nil {
+	// 	fmt.Println("Error fetching Custom Resource:", err)
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// // Extract `spec` field from cr
+	// title, found, err := unstructured.NestedString(cr.Object, "spec", "title")
+	// description, found, err := unstructured.NestedString(cr.Object, "spec", "description")
+	// if err != nil || !found {
+	// 	fmt.Println("Error retrieving spec:", err)
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// Print the spec content
+	// fmt.Println("Spec of", crName, ":", title)
+
+	ghi := &trainingv1alpha1.GithubIssue{}
+	if err := r.Get(ctx, req.NamespacedName, ghi); err != nil {
+		if apiErrors.IsNotFound(err) {
+			// FenceAgentsRemediation CR was not found, and it could have been deleted after reconcile request.
+			// Return and don't requeue
+
+			log.Info("ghi1")
+			fmt.Print(ghi.Spec.Title)
+			fmt.Print(ghi.Spec.Description)
+			// fmt.Print(ghi)
+
+			// Extract `spec` field from cr
+			title := ghi.Spec.Title
+			description := ghi.Spec.Description
+			// if err != nil || !found {
+			// 	fmt.Println("Error retrieving spec:", err)
+			// 	return ctrl.Result{}, nil
+			// }
+
+			var i int
+			var url string
+			url = ""
+			// Print all keys and values dynamically
+			for i = 0; i < len(GitHubIssues); i++ {
+				item := GitHubIssues[i]
+				fmt.Printf("\nIssue %d:\n", i)
+				titleStr := fmt.Sprintf(item["title"].(string))
+				isOpen := fmt.Sprintf(item["state"].(string))
+				url = fmt.Sprintf(item["url"].(string))
+				fmt.Println("url:", url)
+				if (strings.TrimRight(string(titleStr), "\n") == title) && isOpen == "open" {
+					break
+				}
+			}
+
+			log.Info("GitHubIssue CR was not found", "CR Name", req.Name, "CR Namespace", req.Namespace)
+			// log.Info("SHAI", "url:", url, "\ntitle:", title, "\ndescription", description)
+			r.closeGithubIssue(title, description, url)
+			log.Info("Reconciling closeGithubIssue")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get GitHubIssue CR")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("ghi2")
+	fmt.Print(ghi.Spec.Title)
+	fmt.Print(ghi.Spec.Description)
+	// fmt.Print(ghi)
+
+	// Extract `spec` field from cr
+	title := ghi.Spec.Title
+	description := ghi.Spec.Description
+	// if err != nil || !found {
+	// 	fmt.Println("Error retrieving spec:", err)
+	// 	return ctrl.Result{}, nil
+	// }
+
+	var i int
+	// var url string
+	// url = ""
 	// Print all keys and values dynamically
-	for i = 0; i < len(result); i++ {
-		item := result[i]
+	for i = 0; i < len(GitHubIssues); i++ {
+		item := GitHubIssues[i]
 		fmt.Printf("\nIssue %d:\n", i)
 		titleStr := fmt.Sprintf(item["title"].(string))
 		isOpen := fmt.Sprintf(item["state"].(string))
-		// url := fmt.Sprintf(item["url"].(string))
+		// url = fmt.Sprintf(item["url"].(string))
 		if (strings.TrimRight(string(titleStr), "\n") == title) && isOpen == "open" {
 			break
 		}
 	}
 
-	if false{
-		r.closeGithubIssue(title, description, url)
-		log.Info("Reconciling closeGithubIssue")
-	}
-
 	// validate if the requiered GitHub issue is not exists when GitHub issues are empty or not
-	if len(result) == 0 || i == len(result) {
+	if len(GitHubIssues) == 0 || i == len(GitHubIssues) {
 		r.createGithubIssue(title, description)
 		log.Info("Reconciling createGithubIssue")
 	}
+
+	// for _, crd := range crdList.Items {
+	// 	fmt.Printf("- CRD: %s\n", crd.Name)
+
+	// 	var i int
+	// 	var url string
+	// 	url = ""
+
+	// 	// Print conditions (status)
+	// 	if len(crd.Status.Conditions) > 0 {
+	// 		fmt.Println("  Status Conditions:")
+	// 		for _, condition := range crd.Status.Conditions {
+
+	// 			fmt.Printf("    - Type: %s, Status: %s, Reason: %s, Message: %s\n",
+	// 				condition.Type, condition.Status, condition.Reason, condition.Message)
+
+	// 			if condition.Type == "Established" && condition.Status == "True" { // Create new GitHub-issue
+	// 				fmt.Printf("    - Established: %t\n", condition.Type == "Established" && condition.Status == "True")
+
+	// 				// validate if the requiered GitHub issue is not exists when GitHub issues are empty or not
+	// 				if len(GitHubIssues) == 0 || i == len(GitHubIssues) {
+	// 					r.createGithubIssue(title, description)
+	// 					log.Info("Reconciling createGithubIssue")
+	// 				}
+	// 			} else if condition.Type == "Terminating" && condition.Status == "True" { // Close GitHub-issue, CR deleted
+	// 				fmt.Println("  Terminating: True")
+	// 				r.closeGithubIssue(title, description, url)
+	// 				log.Info("Reconciling closeGithubIssue")
+	// 			}
+	// 		}
+	// 	} else {
+	// 		fmt.Println("  Status: No conditions found")
+	// 	}
+	// }
+
+	// // Extract `spec` field
+	// title, found, err := unstructured.NestedString(cr.Object, "spec", "title")
+	// description, found, err := unstructured.NestedString(cr.Object, "spec", "description")
+	// if err != nil || !found {
+	// 	fmt.Println("Error retrieving spec:", err)
+	// 	os.Exit(1)
+	// }
+
+	// // Print the spec content
+	// fmt.Println("Spec of", crName, ":", title)
+
+	// // Fetch issues from GitHub
+	// body, err := r.fetchGitHubIssues()
+	// if err != nil {
+	// 	log.Info("Failed to fetch GitHub issues")
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// // Define a generic map
+	// var result []map[string]interface{}
+
+	// // Parse the JSON
+	// err = json.Unmarshal(body, &result)
+	// if err != nil {
+	// 	log.Info("err\n")
+	// }
+
+	// var i int
+
+	// // Print all keys and values dynamically
+	// for i = 0; i < len(result); i++ {
+	// 	item := result[i]
+	// 	fmt.Printf("\nIssue %d:\n", i)
+	// 	titleStr := fmt.Sprintf(item["title"].(string))
+	// 	isOpen := fmt.Sprintf(item["state"].(string))
+	// 	// url := fmt.Sprintf(item["url"].(string))
+	// 	if (strings.TrimRight(string(titleStr), "\n") == title) && isOpen == "open" {
+	// 		break
+	// 	}
+	// }
+
+	// // if false {
+	// // 	r.closeGithubIssue(title, description, url)
+	// // 	log.Info("Reconciling closeGithubIssue")
+	// // }
+
+	// // validate if the requiered GitHub issue is not exists when GitHub issues are empty or not
+	// if len(result) == 0 || i == len(result) {
+	// 	r.createGithubIssue(title, description)
+	// 	log.Info("Reconciling createGithubIssue")
+	// }
 
 	return ctrl.Result{}, nil
 }
